100999ee3b829fea41b433084ccacfa55cb2f370
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-07-04 19:15:11 +0430

    changes added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ function Home() {
 		<div className=" bg-linear-60 from-red-950 via-red-900 to-red-950 min-h-screen w-full flex justify-center items-center">
 			<div className="border-2 flex justify-center items-center border-dashed animate-pulse border-yellow-200 h-60 w-fit p-10 bg-red-950/60 shadow-2xl shadow-red-950 rounded-full">
 				<h1 className="text-5xl font-black text-yellow-200 ">
-					welcome to home page gy
+					welcome to home page gys
 				</h1>
 			</div>
 		</div>
