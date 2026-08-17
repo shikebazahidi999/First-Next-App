288d871f5c679e025fff8478514893bfd8ff7351
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-08-17 21:53:50 +0430

    one list added

--- src/app/dartworks/App.dart
+++ src/app/dartworks/App.dart
@@ -1,8 +1,27 @@
-string greet(name){
-    return "hello";
-};
 
+
+// // trim( , indexOf, substring, split, replaceAll toLowerCase lenth)
+// void main(){
+
+// string MyName = "Shikeba";
+// MyName.toUpperCase();
+// if(MyName.contains("i")&&MyName.startWith("shi")&&MyName.endsWith("ba")&&MyName.lenth>3){
+//     print("hello miss $MyName");
+
+// }
+// print(MyName.substring(0,6));
+// if(MyName.contains("k")){
+//     print(MyName.indexOf("k"));
+// }
+
+// }
 
 void main(){
-greet();
+    var studentsList = ["Sahar","Fariba","Samira","Frishta","Maryam"];
+    for(num i =0; i<studentsList.lenth; i++){
+        if(studentsList[i].contains("t")){
+ print(studentsList.remove(studentsList[i])); 
+        }
+      
+    }
 }
